7df9e83f04df1d87514a9157f7e8a081441570ff
jason <bjason@gmail.com>
2017-11-04 12:32:00 -0700

    template.

---
+++ script.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d'),
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight;
+
+
+    context.fillRect(200, 200, 50, 50);
