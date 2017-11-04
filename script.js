e57f3147572d52dfe4eaf8094cc86afdf46c31e6
jason <bjason@gmail.com>
2017-11-04 13:24:30 -0700

    cursor

--- script.js
+++ script.js
@@ -13,12 +13,38 @@ var canvas = document.getElementById('canvas'),
     var x = 0;
     var y = 0;
 
+    function intersectRect(r1, r2) {
+      return !(r2.left > r1.right || 
+               r2.right < r1.left || 
+                 r2.top > r1.bottom ||
+                   r2.bottom < r1.top);
+    }
+
+    function collide(r1_x, r1_y, r1_width, r1_height,
+                     r2_x, r2_y, r2_width, r2_height) {
+      var r1 = {
+        left: r1_x,
+        top: r1_y,
+        right: r1_x + r1_width,
+        bottom: r1_y + r1_height,
+      };
+
+      var r2 = {
+        left: r2_x,
+        top: r2_y,
+        right: r2_x + r2_width,
+        bottom: r2_y + r2_height,
+      };
+      return intersectRect(r1, r2);
+    }
+
     function render(){
 
       context.clearRect(0,0, width, height);
-      context.fillRect(x - 20, y - 25, 50, 50);
+      context.fillRect(x , y , 50, 50);
       requestAnimationFrame(render);
     }
+
     $(document).on('mousemove', function(event) {
       x = event.pageX;
       y = event.pageY;
