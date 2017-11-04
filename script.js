2fccfc190e48d31f1272a24e6bd9e6a38228faff
jason <bjason@gmail.com>
2017-11-04 13:18:36 -0700

    volumes.

--- script.js
+++ script.js
@@ -4,15 +4,31 @@ var canvas = document.getElementById('canvas'),
     height = canvas.height = window.innerHeight;
 
 
-    context.fillRect(200, 200, 50, 50);
 
+
+    var colorStep = width / 256;
     // Init 
     var osc = new Tone.Oscillator(440, "sine").toMaster().start();
 
+    var x = 0;
+    var y = 0;
+
+    function render(){
+
+      context.clearRect(0,0, width, height);
+      context.fillRect(x - 20, y - 25, 50, 50);
+      requestAnimationFrame(render);
+    }
     $(document).on('mousemove', function(event) {
-      var x = event.pageX;
+      x = event.pageX;
+      y = event.pageY;
       console.log('event', x);
       osc.frequency.value = x;
+      osc.volume.value = y;
       //the ration between the bpm and the frequency will be maintained
 
+      // Draw 
+
     });
+
+    render();
