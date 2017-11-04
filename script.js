78e7f7697bc9038a4f5c66c177413b1f4337cf65
jason <bjason@gmail.com>
2017-11-04 13:13:18 -0700

    frquency

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ var canvas = document.getElementById('canvas'),
 
 
     context.fillRect(200, 200, 50, 50);
+
+    // Init 
+    var osc = new Tone.Oscillator(440, "sine").toMaster().start();
+
+    $(document).on('mousemove', function(event) {
+      var x = event.pageX;
+      console.log('event', x);
+      osc.frequency.value = x;
+      //the ration between the bpm and the frequency will be maintained
+
+    });
